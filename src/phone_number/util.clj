928e4a76441973e13259e54a3606a1cb0c6deae2
Paweł Wilk <siefca@gnu.org>
2020-05-24 23:50:32 +0200

    Time zone formatting

--- src/phone_number/util.clj
+++ src/phone_number/util.clj
@@ -13,7 +13,8 @@
             geocoding.PhoneNumberOfflineGeocoder
             PhoneNumberToCarrierMapper
             PhoneNumberToTimeZonesMapper
-            NumberParseException]))
+            NumberParseException]
+           [java.time.format TextStyle]))
 
 (defmacro try-parse
   [& body]
@@ -21,23 +22,35 @@
         (catch NumberParseException  e# nil)
         (catch NumberFormatException e# nil)))
 
-(defn ^PhoneNumberUtil              instance         [] (PhoneNumberUtil/getInstance))
-(defn ^PhoneNumberOfflineGeocoder   geo-coder        [] (PhoneNumberOfflineGeocoder/getInstance))
-(defn ^PhoneNumberToCarrierMapper   carrier-mapper   [] (PhoneNumberToCarrierMapper/getInstance))
-(defn ^PhoneNumberToTimeZonesMapper timezones-mapper [] (PhoneNumberToTimeZonesMapper/getInstance))
+;; Singletons
 
-(def formats
+(defn ^PhoneNumberUtil              instance          [] (PhoneNumberUtil/getInstance))
+(defn ^PhoneNumberOfflineGeocoder   geo-coder         [] (PhoneNumberOfflineGeocoder/getInstance))
+(defn ^PhoneNumberToCarrierMapper   carrier-mapper    [] (PhoneNumberToCarrierMapper/getInstance))
+(defn ^PhoneNumberToTimeZonesMapper time-zones-mapper [] (PhoneNumberToTimeZonesMapper/getInstance))
+
+;; Formats
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.IPersistentMap}
+  formats
   "Map of possible format identifiers (keywords) to PhoneNumberFormat values."
   #:format{:e164          PhoneNumberUtil$PhoneNumberFormat/E164
            :international PhoneNumberUtil$PhoneNumberFormat/INTERNATIONAL
            :national      PhoneNumberUtil$PhoneNumberFormat/NATIONAL
            :rfc3966       PhoneNumberUtil$PhoneNumberFormat/RFC3966})
 
-(def formats-by-format
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.IPersistentMap}
+  formats-by-format
   "Map of PhoneNumberFormat values to possible format identifiers (keywords)."
   (clojure.set/map-invert formats))
 
-(def types
+;; Types
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.IPersistentMap}
+  types
   "Map of phone number types (keywords) to PhoneNumberType values."
   {:fixed-line            PhoneNumberUtil$PhoneNumberType/FIXED_LINE
    :mobile                PhoneNumberUtil$PhoneNumberType/MOBILE
@@ -52,6 +65,31 @@
    :voicemail             PhoneNumberUtil$PhoneNumberType/VOICEMAIL
    :unknown               PhoneNumberUtil$PhoneNumberType/UNKNOWN})
 
-(def types-by-type
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.IPersistentMap}
+  types-by-type
   "Map of PhoneNumberType values to phone number types (keywords)."
   (clojure.set/map-invert types))
+
+;; Time Zone Formats
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.IPersistentMap}
+  time-zone-formats
+  "Map of TextStyle objects to time zone formats (keywords) for time zone formatting."
+  {:short                 TextStyle/SHORT
+   :narrow                TextStyle/NARROW
+   :full                  TextStyle/FULL
+   :short-standalone      TextStyle/SHORT_STANDALONE
+   :narrow-standalone     TextStyle/NARROW_STANDALONE
+   :full-standalone       TextStyle/FULL_STANDALONE})
+
+;; Time Zone IDs
+
+(defn zone-id-format
+  "For the given Locale object and TextStyle object renders a string describing a time
+  zone identifier (given as a string) passed as the first argument."
+  {:added "8.12.4-0"
+   :tag String}
+  [^String zone-id ^java.util.Locale l ^TextStyle style]
+  (.getDisplayName (java.time.ZoneId/of zone-id) style l))
