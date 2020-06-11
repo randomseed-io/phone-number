fddeb075a44bbe289a4f1ca97247bbf1e9681a5f
Paweł Wilk <siefca@gnu.org>
2020-06-11 22:57:12 +0200

    Tests updated, added Kaocha dependencies

--- test/phone_number/core_test.clj
+++ test/phone_number/core_test.clj
@@ -7,19 +7,27 @@
 
     phone-number.core-test
 
-  (:require [clojure.spec.alpha      :as            s]
-            [midje.sweet             :refer      :all]
-            [midje.experimental      :refer [for-all]]
-            [clojure.spec.gen.alpha  :as          gen]
-            [clojure.spec.test.alpha :as           st]
-            ;;[orchestra.spec.test     :as           ot]
-            [phone-number.core       :refer      :all]
-            [phone-number.spec       :as         spec]))
+  (:refer-clojure :exclude [format type])
+
+  (:require [clojure.spec.alpha      :as             s]
+            [midje.sweet             :refer       :all]
+            [midje.experimental      :refer  [for-all]]
+            [clojure.spec.gen.alpha  :as           gen]
+            [clojure.spec.test.alpha :as            st]
+            ;;[orchestra.spec.test     :as          ot]
+            [phone-number.core       :refer       :all]
+            [phone-number.spec       :as          spec])
+
+  (:import  [com.google.i18n.phonenumbers
+             Phonenumber$PhoneNumber]))
 
 (s/check-asserts true)
 
-;; (facts "about `trim-both-onc`"
-;;        (fact "when it returns nil for nil"
-;;              (trim-both-once nil)          => nil
-;;              (trim-both-once #{} nil)      => nil
-;;              (trim-both-once \a \b nil)    => nil))
+(facts "about `number`"
+       (fact "when it returns nil for nil"
+             (number nil) => nil)
+       (fact "when it returns PhoneNumber object for a string"
+             (class (number "+48 998"))                            => Phonenumber$PhoneNumber
+             (class (number "+448081570001"))                      => Phonenumber$PhoneNumber
+             (class (number "8081570001" :gb))                     => Phonenumber$PhoneNumber
+             (class (number "8081570001" :phone-number.region/gb)) => Phonenumber$PhoneNumber))
