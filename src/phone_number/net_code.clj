b994b0b29f445f2e9a2ced8817df1f07e7cc6132
Paweł Wilk <siefca@gnu.org>
2021-01-26 16:30:23 +0100

    Added pro-forma -arg variants of possible net codes and their validation function

--- src/phone_number/net_code.clj
+++ src/phone_number/net_code.clj
@@ -27,13 +27,13 @@
 
 (def ^{:added "8.12.4-0"
        :tag clojure.lang.PersistentHashSet}
-  all
-  "Set of supported global network calling codes."
+  all-arg
+  "Set of supported global network calling codes (to be used as arguments)."
   (set (.getSupportedGlobalNetworkCallingCodes (util/instance))))
 
 (def ^{:added "8.12.4-0"
        :tag clojure.lang.PersistentHashSet}
-  by-val all)
+  by-val-arg all-arg)
 
 (def ^{:added "8.12.4-0"
        :const true
@@ -44,6 +44,19 @@
        :const true
        :tag Integer}
   default-val 0)
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.PersistentHashSet}
+  all
+  "Set of supported global network calling codes (including unknown)."
+  (conj
+   (set (.getSupportedGlobalNetworkCallingCodes (util/instance)))
+   default))
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.PersistentHashSet}
+  by-val
+  (conj by-val-arg default-val))
 
 (def ^{:added "8.12.4-0"
        :tag clojure.lang.PersistentVector}
@@ -57,6 +70,18 @@
   "Vector of all supported global network calling codes."
   all-vec)
 
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.PersistentVector}
+  all-arg-vec
+  "Vector of all supported global network calling codes (valid as args)."
+  (vec all-arg))
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.PersistentVector}
+  by-val-arg-vec
+  "Vector of all supported global network calling codes (valid as args)."
+  all-arg-vec)
+
 (defn valid?
   "Returns true if the given region-specification is a valid region code, false
   otherwise."
@@ -64,12 +89,19 @@
   [^Integer calling-code]
   (contains? all calling-code))
 
+(defn valid-arg?
+  "Returns true if the given region-specification is a valid region code (to be used as
+  an argument), false otherwise."
+  {:added "8.12.4-0" :tag Boolean}
+  [^Integer calling-code]
+  (contains? all-arg calling-code))
+
 (defn parse
   "Parses a network calling code and returns a value that can be supplied to
   Libphonenumber methods."
   {:added "8.12.4-0" :tag Integer}
   ([^Integer calling-code]
-   (assert (valid? calling-code)
+   (assert (valid-arg? calling-code)
            (str "Global network calling code " calling-code " is not valid"))
    calling-code))
 
@@ -78,3 +110,21 @@
   {:added "8.12.4-0" :tag Integer}
   ([] (rand-nth all-vec))
   ([^java.util.Random rng] (util/get-rand-nth all-vec rng)))
+
+(defn generate-sample-val
+  "Generates random number type."
+  {:added "8.12.4-0" :tag Integer}
+  ([] (rand-nth by-val-vec))
+  ([^java.util.Random rng] (util/get-rand-nth by-val-vec rng)))
+
+(defn generate-arg-sample
+  "Generates random number type."
+  {:added "8.12.4-0" :tag Integer}
+  ([] (rand-nth all-arg-vec))
+  ([^java.util.Random rng] (util/get-rand-nth all-arg-vec rng)))
+
+(defn generate-arg-sample-val
+  "Generates random number type."
+  {:added "8.12.4-0" :tag Integer}
+  ([] (rand-nth by-val-arg-vec))
+  ([^java.util.Random rng] (util/get-rand-nth by-val-arg-vec rng)))
