d99c3d65775bcf59949d76ddc88208cce20c990e
Paweł Wilk <siefca@gnu.org>
2021-01-27 10:46:22 +0100

    Added missing generators and sets, validation improved

--- src/phone_number/leniency.clj
+++ src/phone_number/leniency.clj
@@ -20,11 +20,23 @@
       :strict   PhoneNumberUtil$Leniency/STRICT_GROUPING
       :valid    PhoneNumberUtil$Leniency/VALID})
 
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentVector}
+  all-arg
+  "Vector of leniencies (keywords) to be used as arguments."
+  all)
+
 (def ^{:added "8.12.4-3"
        :tag clojure.lang.PersistentVector}
   all-vec
   "Vector of leniencies (keywords)."
   (vec (keys all)))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentVector}
+  all-arg-vec
+  "Vector of leniencies (keywords) to be used as arguments."
+  (vec (keys all-arg)))
 
 (def ^{:added "8.12.4-3"
        :tag clojure.lang.PersistentArrayMap}
@@ -32,11 +44,23 @@
   "Map of Leniency values to leniencies (keywords)."
   (clojure.set/map-invert all))
 
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentArrayMap}
+  by-val-arg
+  "Map of Leniency values to leniencies (keywords) to be used as arguments."
+  (clojure.set/map-invert all-arg))
+
 (def ^{:added "8.12.4-3"
        :tag clojure.lang.PersistentVector}
   by-val-vec
   "Vector of leniencies (Leniency values)."
   (vec (keys by-val)))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentVector}
+  by-val-arg-vec
+  "Vector of leniencies (Leniency values) to be used as arguments."
+  (vec (keys by-val-arg)))
 
 (def ^{:added "8.12.4-3"
        :const true
@@ -57,6 +81,16 @@
     ^Boolean use-infer]
    (contains? all (util/ns-infer "phone-number.leniency" leniency use-infer))))
 
+(defn valid-arg?
+  "Returns true if the given leniency is a valid leniency to be used as an argument,
+  false otherwise. In its binary form it uses namespace inference."
+  {:added "8.12.16-1" :tag Boolean}
+  ([^clojure.lang.Keyword leniency]
+   (contains? all-arg leniency))
+  ([^clojure.lang.Keyword leniency
+    ^Boolean use-infer]
+   (contains? all-arg (util/ns-infer "phone-number.leniency" leniency use-infer))))
+
 (defn parse
   "Parses a leniency and returns a value that can be supplied to
   Libphonenumber methods."
@@ -68,7 +102,7 @@
    (if (nil? k)
      default-val
      (let [k (util/ns-infer "phone-number.leniency" k use-infer)]
-       (assert (valid? k) (str "Leniency specification " k " is not valid"))
+       (assert (valid-arg? k) (str "Leniency specification " k " is not valid"))
        (all k)))))
 
 (defn generate-sample
@@ -77,8 +111,20 @@
   ([] (rand-nth all-vec))
   ([^java.util.Random rng] (util/get-rand-nth all-vec rng)))
 
+(defn generate-sample-arg
+  "Generates random leniency (suitable as an argument)."
+  {:added "8.12.16-1" :tag clojure.lang.Keyword}
+  ([] (rand-nth all-arg-vec))
+  ([^java.util.Random rng] (util/get-rand-nth all-arg-vec rng)))
+
 (defn generate-sample-val
   "Generates random leniency (string value)."
   {:added "8.12.4-3" :tag String}
   ([] (rand-nth by-val-vec))
   ([^java.util.Random rng] (util/get-rand-nth by-val-vec rng)))
+
+(defn generate-sample-val-arg
+  "Generates random leniency (string value, suitable as an argument)."
+  {:added "8.12.16-1" :tag String}
+  ([] (rand-nth by-val-arg-vec))
+  ([^java.util.Random rng] (util/get-rand-nth by-val-arg-vec rng)))
