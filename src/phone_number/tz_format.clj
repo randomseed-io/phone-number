4a699d973e219428a50a25489d7c6e0120a4cb38
Paweł Wilk <siefca@gnu.org>
2021-01-27 12:03:57 +0100

    Formats handling updated with data structures for sampling and validation

--- src/phone_number/tz_format.clj
+++ src/phone_number/tz_format.clj
@@ -15,7 +15,7 @@
 (def ^{:added "8.12.4-0"
        :tag clojure.lang.PersistentArrayMap}
   all
-  "Map of TextStyle objects to time zone formats (keywords) for time zone formatting."
+  "Map of time zone formats (keywords) to TextStyle objects for time zone formatting."
   #::{:id                nil
       :short             TextStyle/SHORT
       :narrow            TextStyle/NARROW
@@ -25,12 +25,6 @@
       :full-standalone   TextStyle/FULL_STANDALONE})
 
 (def ^{:added "8.12.4-0"
-       :tag clojure.lang.PersistentVector}
-  all-vec
-  "Vector of time zone formats (keywords)."
-  (vec (keys all)))
-
-(def ^{:added "8.12.4-0"
        :const true
        :tag clojure.lang.Keyword}
   default ::id)
@@ -38,6 +32,51 @@
 (def ^{:added "8.12.4-0"
        :tag TextStyle}
   default-val (all default))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentMap}
+  by-val
+  "Map of time zone formats to TextStyle objects."
+  (clojure.set/map-invert all))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentMap}
+  all-arg
+  "Map of TextStyle objects to time zone formats (keywords) for time zone
+  formatting (to be used as arguments)."
+  all)
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentMap}
+  by-val-arg
+  "Map of time zone formats as TextStyle objects mapped to keywords (values accepted by
+  Libphonenumber methods)."
+  (dissoc by-val default-val))
+
+(def ^{:added "8.12.4-0"
+       :tag clojure.lang.PersistentVector}
+  all-vec
+  "Vector of time zone formats (keywords)."
+  (vec (keys all)))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentVector}
+  all-arg-vec
+  "Vector of time zone formats (keywords) to be used as arguments."
+  (vec (keys all-arg)))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentVector}
+  by-val-vec
+  "Vector of time zone values."
+  (vec (keys by-val)))
+
+(def ^{:added "8.12.16-1"
+       :tag clojure.lang.PersistentVector}
+  by-val-arg-vec
+  "Vector of time zone values suitable to be passed as arguments to methods of
+  Libphonenumber."
+  (vec (keys by-val-arg)))
 
 ;; Time Zone IDs
 
@@ -50,6 +89,15 @@
     ^Boolean use-infer]
    (contains? all (util/ns-infer "phone-number.tz-format" tz-format use-infer))))
 
+(defn valid-arg?
+  "Returns true if the given tz-format is valid as an argument, false otherwise."
+  {:added "8.12.16-1" :tag Boolean}
+  ([^clojure.lang.Keyword tz-format]
+   (contains? all-arg tz-format))
+  ([^clojure.lang.Keyword tz-format
+    ^Boolean use-infer]
+   (contains? all-arg (util/ns-infer "phone-number.tz-format" tz-format use-infer))))
+
 (defn transform
   "For the given `Locale` object and `TextStyle` object renders a string describing a
   time zone identifier (given as a string) passed as the first argument. If the style
@@ -59,7 +107,7 @@
   [^String           zone-id
    ^java.util.Locale l
    ^TextStyle        style]
-  (if (nil? style)
+  (if (or (nil? style) (= default-val style))
     zone-id
     (.getDisplayName (java.time.ZoneId/of zone-id) style l)))
 
@@ -74,5 +122,5 @@
    (if (nil? k)
      default-val
      (let [k (util/ns-infer "phone-number.tz-format" k use-infer)]
-       (assert (valid? k) (str "Time zone format " k " is not valid"))
+       (assert (valid-arg? k) (str "Time zone format " k " is not valid"))
        (all k)))))
